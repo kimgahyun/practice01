5d564ba5b28dc7851ee4892a402ddecd5e2499d2
kickscar <kickscar@gmail.com>
2016-09-13 15:45:42 +0900

    solved!

--- src/practice01/Game369.java
+++ src/practice01/Game369.java
@@ -1,8 +1,30 @@
 package practice01;
 
 public class Game369 {
-
 	public static void main(String[] args) {
-
+		
+		for( int i = 1; i <= 99; i++ ) {
+			String s = String.valueOf( i );
+			
+			int count = 0;
+			int length = s.length();
+			for( int j = 0; j < length; j++ ) {
+				char c = s.charAt( j ); 
+				if( c == '3' || c == '6' || c == '9' ) {
+					count ++;
+				}
+			}
+			
+			if( count == 0 ) {
+				continue;
+			}
+			
+			System.out.print( i + " ");
+			for( int j = 0; j < count; j++ ) {
+				System.out.print( "짝" );
+			}
+			
+			System.out.println( "" );
+		}
 	}
 }
--- src/practice01/GuessNumber.java
+++ src/practice01/GuessNumber.java
@@ -1,7 +1,43 @@
 package practice01;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class GuessNumber {
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		while (true) {
+			
+			int min = 1;
+			int max = 100;
+			Random random = new Random();
+			int correctNumber = random.nextInt(max) + min;
+			
+			while (true) {
+				System.out.print(">>");
+				int answerNumber = scanner.nextInt();
 
+				if (answerNumber < correctNumber) {
+					System.out.println("더 높게");
+					min = answerNumber;
+				} else if (answerNumber > correctNumber) {
+					System.out.println("더 낮게");
+					max = answerNumber;
+				} else {
+					System.out.println("맞았습니다.");
+					break;
+				}
+
+				System.out.println(min + "-" + max);
+			}
+
+			System.out.print("게임을 종료 하시겠습니까?(y/n) >>");
+			String retryAnswer = scanner.next();
+			if ("y".equals(retryAnswer)) {
+				break;
+			}
+		}
+
+		scanner.close();
 	}
 }
